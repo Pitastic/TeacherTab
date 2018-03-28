ba4affe56e3f1cd4791affb98aa03e5e9f211eeb
Pitastic <mickep1985@gmail.com>
2018-03-28 17:29:23 +0200

    zwei von vier Exportfunktionen implementiert ! closes #32

--- js/export.js
+++ js/export.js
@@ -1,32 +1,56 @@
-/*
-- Hinweise einblenden:
-	- PopUps zu erlauben
-	- Querformat
-	- auf Papier oder PDF drucken
-*/
-
 $(document).ready(function() {
 
 	GLOBALS.mndl = [];
 	GLOBALS.fspz = [];
 	GLOBALS.schr = [];
 
+
 	// Funktionen, die auf global SETTINGS warten müssen
 	db_readMultiData(function(r){
 		// Settings laden
 		SETTINGS = r[0];
 
-		db_readMultiData(function(rows){
-			writeAllgemeines(rows);
+		// Ausgabe des Exports
+		switch(sessionStorage.getItem("export_type")){
+			case "csv":
+			case "json":
 
-			db_readMultiData(function(rows){
-				writeGesamtuebersicht(rows);
-				writeLeistungen(rows);
-			}, "student");
+				document.getElementById("bezeichnung").innerHTML = SETTINGS.name;
+				
+				db_readKlasse(function(r){
 
-		}, "leistung");
+					var jsonData = btoa(JSON.stringify(r[1]));
+
+					var a = document.createElement('a');
+					a.href = 'data:application/json;charset=utf-8;base64,' + jsonData;
+					a.download = GLOBALS.klasse+'.json';
+
+					// Append - Click - Remove
+					document.body.appendChild(a);
+					a.click();
+					document.body.removeChild(a);
+
+				});
+				break;
+
+			case "copy":
+
+			default:
+				// Immer HTML wenn nicht anders gefordert
+				db_readMultiData(function(rows){
+					writeAllgemeines(rows);
+
+					db_readMultiData(function(rows){
+						writeGesamtuebersicht(rows);
+						writeLeistungen(rows);
+					}, "student");
+
+				}, "leistung");
+
+		}
 
 	}, "settings");
+
 
 	closeListener();
 
@@ -219,4 +243,140 @@ function zensur(zeilen_objekt, spezKey, digit){
 		result = result[spezKey];
 	}
 	return Math.round(result*digit)/digit || "-"
+}
+
+function createCSV(results) {
+	var columns=['mndl','fspz','schr'];
+	var nameDict={};
+	var buffer = "";
+	var buffer_row="";
+	var tempO, row, i;
+	for (var i_outer = 0; i_outer < results.rows.length; i_outer++) {
+		buffer_row = "";
+		row = results.rows.item(i_outer);
+		if (!row.id) {
+			// - Gesamtuebersicht Header
+			if (row.mndl){
+				var mndl = JSON.parse(decodeURIComponent(row.mndl));
+				var lengthMndl = 0;
+				for (i=0; i<mndl.alle.length;i++){
+					nameDict[mndl.alle[i]] = mndl[mndl.alle[i]].Bezeichnung;
+					lengthMndl++;
+				}
+			}
+			if (row.mndl){
+				var fspz = JSON.parse(decodeURIComponent(row.fspz));
+				var lengthFspz = 0;
+				for (i=0; i<fspz.alle.length;i++){
+					nameDict[fspz.alle[i]] = fspz[fspz.alle[i]].Bezeichnung;
+					lengthFspz++;
+				}
+			}
+			if (row.schr){
+				var schr = JSON.parse(decodeURIComponent(row.schr));
+				var lengthSchr = 0;
+				for (i=0; i<schr.alle.length;i++){
+					nameDict[schr.alle[i]] = schr[schr.alle[i]].Bezeichnung;
+					lengthSchr++;
+				}
+			}
+			buffer_row += "Gesamtübersicht\n\n;";
+			buffer_row += Array(lengthMndl+1).join("m;");
+			buffer_row += Array(lengthFspz+1).join("f;")+"(fspz DS);";
+			buffer_row += "mndl (DS);";
+			buffer_row += Array(lengthSchr+1).join("s;")+"schr (DS);rechnerisch;Gesamt";
+		}else{
+			// - Gesamtuebersicht Daten
+			buffer_row += row.nName+", "+row.vName+";";
+			tempO = JSON.parse(decodeURIComponent(row.mndl));
+			for (var key in tempO) {
+				if (tempO[key] && !tempO[key].length){
+					buffer_row += (tempO[key].Mitschreiber && tempO[key].Mitschreiber !== "false" && tempO[key].Mitschreiber !== "undefined") ? tempO[key].Note.toString().replace(".",",")+";" : "-;";
+				}
+			}
+			tempO = JSON.parse(decodeURIComponent(row.fspz));
+			for (key in tempO) {
+				if (tempO[key] && !tempO[key].length){
+					buffer_row += (tempO[key].Mitschreiber && tempO[key].Mitschreiber !== "false" && tempO[key].Mitschreiber !== "undefined") ? tempO[key].Note.toString().replace(".",",")+";" : "-;";
+				}
+			}
+			buffer_row += JSON.parse(decodeURIComponent(row.ofspz)).Gesamt.toString().replace(".",",")+";";
+			buffer_row += row.omndl.toString().replace(".",",")+";";
+			tempO = JSON.parse(decodeURIComponent(row.schr));
+			for (key in tempO) {
+				if (key !== "alle" && tempO[key]){
+					buffer_row += (tempO[key].Mitschreiber && tempO[key].Mitschreiber !== "false" && tempO[key].Mitschreiber !== "undefined") ? tempO[key].Note.toString().replace(".",",")+";" : "-;";
+				}
+			}
+			buffer_row += row.oschr.toString().replace(".",",")+";";
+			buffer_row += JSON.parse(decodeURIComponent(row.gesamt)).rechnerisch.toPrecision(3)+";";
+			buffer_row += JSON.parse(decodeURIComponent(row.gesamt)).eingetragen+";";
+		}
+		// cache Data bis hier hin
+		buffer += buffer_row+"%0A";
+	}
+	var eintragung;
+	var Leistung;
+	buffer += "%0A";
+	for (var i=0; i<columns.length;i++){
+		eintragung = null;
+		row = results.rows.item(0);
+		var oHeader = JSON.parse(decodeURIComponent(row[columns[i]]));
+		for (var i2=0;i2<oHeader.alle.length;i2++){
+			buffer_row = "";
+			var _id = oHeader.alle[i2];
+			// Header-Daten
+			eintragung = oHeader[_id].Eintragung;
+			buffer_row += oHeader[_id].Bezeichnung+";("+[columns[i]]+");;Art :;"+eintragung+";\n"+oHeader[_id].Datum+";%0A";
+			if (eintragung == "Rohpunkte"){
+				buffer_row += ";;Kat. 1;Kat. 2;Kat. 3;Kat. 4;Gesamt;Prozent;Note;%0A";
+				for (var v in oHeader[_id].Verteilungen){
+					buffer_row += "Verteilung: "+oHeader[_id].Verteilungen[v]+";;"+oHeader[_id][oHeader[_id].Verteilungen[v]].Kat1+";"+oHeader[_id][oHeader[_id].Verteilungen[v]].Kat2+";"+oHeader[_id][oHeader[_id].Verteilungen[v]].Kat3+";"+oHeader[_id][oHeader[_id].Verteilungen[v]].Kat4+";"+oHeader[_id][oHeader[_id].Verteilungen[v]].Gesamt+";-;-;%0A";
+				}
+			}else if (eintragung == "Punkte") {
+				var maxPts = oHeader[_id].Standard.Gesamt;
+				buffer_row += ";;Max.;"+maxPts+";;Punkte;Note;;;%0A";
+			}else{
+				buffer_row += ";;Note;;;;;;;%0A";
+			}
+			buffer_row += "%0A";
+			// Schüler-Daten
+			for (i_outer = 0; i_outer < results.rows.length; i_outer++) {
+				row = results.rows.item(i_outer);
+				if (row.id){
+					tempO = JSON.parse(decodeURIComponent(row[columns[i]]))[_id] || {};
+					if (eintragung=="Noten"){
+						Leistung = tempO.Note || "-";
+						buffer_row += row.nName+", "+row.vName+";;"+Leistung+";;;;;;;%0A";
+					}else if (eintragung=="Punkte"){
+						Leistung = (tempO.Mitschreiber == "true") ? ";;;"+tempO.Gesamt+";"+tempO.Note+";;;" : ";;;-;-;;;";
+						buffer_row += row.nName+", "+row.vName+";;"+Leistung+";%0A";                                    	
+					}else{
+						Leistung = (tempO.Mitschreiber == "true") ? tempO.Kat1+";"+tempO.Kat2+";"+tempO.Kat3+";"+tempO.Kat4+";"+tempO.Gesamt+";"+tempO.Prozent.replace(".",",")+";"+tempO.Note+";"+tempO.Verteilung : "-;-;-;-;-;-;-;-";
+						buffer_row += row.nName+", "+row.vName+";;"+Leistung+";%0A";
+					}
+				}
+			}
+			buffer += "%0A%0A%0A"+buffer_row;
+		}
+		
+	}
+	// download stuff
+	// - Datei Download
+	var link = document.getElementById('export_to_csv');
+	var fileName = "export.csv";
+	if (true) {
+	//if(link.download !== undefined) { // feature detection
+		// Browsers that support HTML5 download attribute
+		// Replace NewLine Zeichen
+		buffer = buffer.replace(/%0A/g,"\n");
+		var blob = new Blob([buffer], {
+			"type": "text/csv;charset=utf8;"			
+		});
+		link.setAttribute("href", window.URL.createObjectURL(blob));
+		link.setAttribute("download", fileName);
+	 } else {
+		// Weiterleitung mit GET-Übergabe
+		link.setAttribute("href", link.getAttribute('href')+"?csv="+escape(buffer));
+	}
 }
