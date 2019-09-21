d754d7d0370a20fbd213f7a62d1ebcdafea32439
Pitastic <mickep1985@gmail.com>
2019-09-21 21:30:17 +0200

    Set Version für Release

--- src/js/identify.js
+++ src/js/identify.js
@@ -30,7 +30,7 @@ var GLOBALS = {
 	'timeout': 20000,
 	'unlimited_dates': ["2099-01-01", "2098-01-01"],
 
-	'appversion': "2.0.1",
+	'appversion': "2.1",
 	'up2date': true,
 	'dbname': null,
 	'dbversion': null,
